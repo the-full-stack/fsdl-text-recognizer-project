46ee4232096f6c106e0700b8cb80ff0ce33391e9
Sergey Karayev <sergeykarayev@gmail.com>
2019-03-01 14:49:25 -0800

    add iam

--- lab4_sln/text_recognizer/networks/__init__.py
+++ lab4_sln/text_recognizer/networks/__init__.py
@@ -1,9 +1,9 @@
 from .mlp import mlp
 # Hide lines below until Lab 2
 from .lenet import lenet
+from .line_cnn_all_conv import line_cnn_all_conv
 # Hide lines above until Lab 2
 # Hide lines below until Lab 3
-from .line_cnn_all_conv import line_cnn_all_conv
 from .line_lstm_ctc import line_lstm_ctc
 # Hide lines above until Lab 3
 
